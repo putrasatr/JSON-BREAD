a3ccbaf08b04ef70ce4111264094f8f71c879aeb
putrasatr <satriaputrapratama2002@gmail.com>
2021-03-14 14:34:20 +0700

    Filter

--- app.js
+++ app.js
@@ -26,58 +26,53 @@ app.use(express.urlencoded({
 
 //Rendering Home
 app.get('/', function (req, res, next) {
+    const page = req.params.page || 1
+    const limit = 3
+    const offset = (page - 1) * limit;
+    const url = req.url == '/' ? '/?page=1' : req.url
+
+    const { index, string, integer, float, boolean } = req.query
+
+    let dataFiltered = [];
+    let onSearch = false
+    data.map((item, idx) => {
+        if (item.id == index) {
+            onSearch = true
+            dataFiltered.push(item)
+        }
+        if (item.string == string) {
+            onSearch = true
+            dataFiltered.push(item)
+        }
+        if (item.integer == integer) {
+            onSearch = true
+            dataFiltered.push(item)
+        }
+        if (item.float == float) {
+            onSearch = true
+            dataFiltered.push(item)
+        }
+        if (item.boolean.toString() == boolean && boolean != 'none') {
+            onSearch = true
+            dataFiltered.push(item)
+        }
+    })
+    const total = data.length;
+    const i = data.splice(offset, limit)
+
+    const pages = Math.ceil(total / limit);
+
     res.render('index.ejs', {
-        data
+        dataFiltered,
+        i,
+        data,
+        onSearch,
+        page,
+        pages,
+        url
     })
 
 });
-
-// router.get('/', function (req, res, next) {
-//     const page = req.query.page || 1
-//     const url = req.url == '/' ? '/?page=1' : req.url
-//     const limit = 3
-//     const offset = (page - 1) * limit
-
-//     let params = []
-//     let paramsSql = []
-
-//     if (req.query.nama) {
-//       params.push(req.query.nama)
-//       paramsSql.push(`nama ilike '%' || $${params.length} || '%'`)
-//     }
-
-//     if (req.query.umur) {
-//       params.push(Number(req.query.umur))
-//       paramsSql.push(`umur = $${params.length}`)
-//     }
-
-//     if (req.query.isjomblo) {
-//       params.push(JSON.parse(req.query.isjomblo))
-//       paramsSql.push(`isjomblo = $${params.length}`)
-//     }
-
-//     let sql = `SELECT count(*) as total FROM murid`
-//     if (params.length > 0) sql += ` where ${paramsSql.join(' AND ')}`
-//     console.log(sql)
-
-//     pool.query(sql, params, (err, data) => {
-//       if (err) return res.send('error pertama')
-//       const total = data.rows[0].total;
-//       const pages = Math.ceil(total / limit)
-
-
-//       sql = sql.replace('count(*) as total', '*');
-//       params.push(limit)
-//       params.push(offset)
-//       sql += ` limit $${params.length - 1} offset $${params.length}`
-//       console.log(sql)
-
-//       pool.query(sql, params, (err, data) => {
-//         if (err) return res.send(err)
-//         res.render('index', { data: data.rows, page, pages, query: req.query, url })
-//       })
-//     })
-//   });
 
 //Add 
 app.get('/add', function (req, res, next) {
@@ -85,7 +80,9 @@ app.get('/add', function (req, res, next) {
 });
 
 app.post('/add', function (req, res) {
+    let count = data[data.length - 1].id
     data.push({
+        id: Number(count) + 1,
         string: req.body.string,
         integer: parseInt(req.body.integer),
         float: parseFloat(req.body.float),
@@ -99,15 +96,24 @@ app.post('/add', function (req, res) {
 //Edit
 app.get('/edit/:id', function (req, res, next) {
     let id = req.params.id;
+    let item;
+    data.map((items, index) => {
+        if (items.id == id) item = items
+    })
     res.render('edit.ejs', {
-        item: data[id],
-        id: id
+        item,
+        id
     });
 });
 
 app.post('/edit/:id', function (req, res) {
     let idx = req.params.id;
-    data[idx] = {
+    let id;
+    data.map((item, index) => {
+        if (item.id == idx) id = index
+    })
+    data[id] = {
+        id: idx,
         string: req.body.string,
         integer: parseInt(req.body.integer),
         float: parseFloat(req.body.float),
@@ -119,73 +125,14 @@ app.post('/edit/:id', function (req, res) {
 })
 
 
-app.post('/search', (req, res) => {
-    const page = req.query.page || 1;
-    const limit = 5;
-    const offset = (page - 1) * limit;
-    const url = req.url == '/' ? '/?page=1' : req.url
-
-    let params = [];
-    let isFilter = false;
-
-    if (req.query.checkid && req.query.formid) {
-        params.push(`id=${req.query.formid}`);
-        isFilter = true;
-    }
-    if (req.query.checkstring && req.query.formstring) {
-        params.push(`string like '%${req.query.formstring}%'`);
-        isFilter = true;
-    }
-    if (req.query.checkinteger && req.query.forminteger) {
-        params.push(`integer=${req.query.forminteger}`);
-        isFilter = true;
-    }
-    if (req.query.checkfloat && req.query.formfloat) {
-        params.push(`float=${req.query.formfloat}`);
-        isFilter = true;
-    }
-    if (req.query.checkdate && req.query.formdate && req.query.formenddate) {
-        params.push(`date between '${req.query.formdate}' and '${req.query.formenddate}'`);
-        isFilter = true;
-    }
-    if (req.query.checkboolean && req.query.boolean) {
-        params.push(`boolean='${req.query.boolean}'`);
-        isFilter = true;
-    }
-
-    let sql = `select count(*) as total from users`;
-    if (isFilter) {
-        sql += ` where ${params.join(' and ')}`
-        console.log(params)
-    }
-
-    app.get(sql, (err, count) => {
-        const total = count.total;
-        console.log(count.total)
-        const pages = Math.ceil(total / limit);
-        sql = `select * from users`;
-        if (isFilter) {
-            sql += ` where ${params.join(' and ')}`
-        }
-        sql += ` limit ${limit} offset ${offset}`;
-
-        db.all(sql, (err, rows) => {
-            res.render('index.ejs', {
-                rows,
-                page,
-                pages,
-                query: req.query,
-                url
-            });
-        });
-    });
-
-});
-
 //Delete
 app.get('/delete/:id', function (req, res, next) {
     let id = req.params.id;
-    data.splice(id, 1);
+    let idx;
+    data.map((item, index) => {
+        if (item.id == id) idx = index
+    })
+    data.splice(idx, 1);
     writeData(data);
     res.redirect('/');
 });
