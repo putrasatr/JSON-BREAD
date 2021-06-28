29c214ebf03a926254bbf668b1dd53b50cb0e700
putrasatr <satriaputrapratama2002@gmail.com>
2021-06-28 08:46:24 +0700

    filter by limit and sorting feature done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express()
 const fs = require('fs')
 const bodyParser = require('body-parser');
-const { getDataFiltered } = require('./helpers');
+const { getDataFiltered, getSortedData, generateIndex, getKasKus } = require('./helpers');
 const port = 3000
 
 
@@ -29,8 +29,24 @@ app.get('/', function (req, res, next) {
     let data = JSON.parse(fs.readFileSync(path.join(__dirname, './db/data.json'), 'utf8'));
     let page = req.query.page || 1
     let params = []
+    let paramsDate = []
+    const listSorter = [
+        { value: 'id', name: 'ID' },
+        { value: 'string', name: 'String' },
+        { value: 'integer', name: 'Integer' },
+        { value: 'float', name: 'Float' },
+        { value: 'date', name: 'Date' }
+    ]
+    const listLimit = [
+        { value: 5, name: 5 },
+        { value: 10, name: 10 },
+        { value: 15, name: 15 },
+        { value: 20, name: 20 },
+        { value: "All", name: "All" }
+    ]
+    generateIndex(data)
     const url = req.url == '/' ? "/?page=1" : req.url
-    const limit = 5;
+    const limit = req.query.limit == 'All' ? data.length : req.query.limit || 5;
     const offset = (page - 1) * limit
     const {
         index,
@@ -45,42 +61,45 @@ app.get('/', function (req, res, next) {
         end_date,
         checkDate,
         boolean,
-        checkBoolean
+        checkBoolean,
+        search,
+        sorter
     } = req.query
-    if (index && checkid) {
-        params.length > 0 ? params[0]["id"] = index : params.push({ id: index })
+    if (sorter != "id" && sorter) {
+        data = getSortedData(data, sorter)[0]
     }
-    if (string && checkString) {
-        params.length > 0 ? params[0]["string"] = string : params.push({ string })
-    }
-    if (integer && checkInteger) {
-        params.length > 0 ? params[0]["integer"] = integer : params.push({ integer })
-    }
-    if (float && checkFloat) {
-        params.length > 0 ? params[0]["float"] = float : params.push({ float })
+    if (search) {
+        if (index && checkid) {
+            params.length > 0 ? params[0]["id"] = index : params.push({ id: index })
+        }
+        if (string && checkString) {
+            params.length > 0 ? params[0]["string"] = string : params.push({ string })
+        }
+        if (integer && checkInteger) {
+            params.length > 0 ? params[0]["integer"] = integer : params.push({ integer })
+        }
+        if (float && checkFloat) {
+            params.length > 0 ? params[0]["float"] = float : params.push({ float })
 
-    }
-    if (start_date && checkDate) {
-        params.length > 0 ? params[0]["start_date"] = start_date : params.push({ start_date })
-
-    }
-    if (end_date && checkDate) {
-        params.length > 0 ? params[0]["end_date"] = end_date : params.push({ end_date })
-
-    }
-    if (boolean && checkBoolean) {
-        params.length > 0 ? params[0]["boolean"] = boolean : params.push({ boolean })
+        }
+        if ((start_date || end_date) && checkDate) {
+            paramsDate.push(start_date || '0000-00-00', end_date || "9999-99-99")
+        }
+        if (boolean && checkBoolean) {
+            params.length > 0 ? params[0]["boolean"] = boolean : params.push({ boolean })
+        }
     }
     let total;
     let dataFiltered;
-    if (params.length > 0) {
+    if (params.length > 0 || paramsDate.length > 0) {
         let result = []
-        total = getDataFiltered(data, params).length
-        dataFiltered = getDataFiltered(data, params).splice(offset, limit)
+        total = getDataFiltered(data, params[0] ? params : [{}], paramsDate).length
+        dataFiltered = getDataFiltered(data, params[0] ? params : [{}], paramsDate).splice(offset, limit)
     } else {
         total = data.length
-        dataFiltered = data.map((i, idx) => { i["index"] = idx; return i }).splice(offset, limit)
+        dataFiltered = data.splice(offset, limit)
     }
+    // console.log(paramsDate)
     const pages = Math.ceil(total / limit)
     res.render('index.ejs', {
         data: dataFiltered,
@@ -89,7 +108,9 @@ app.get('/', function (req, res, next) {
         pages,
         page: parseInt(page),
         limit,
-        offset
+        offset,
+        listSorter,
+        listLimit
     })
 
 });
@@ -106,7 +127,7 @@ app.post('/add', function (req, res) {
         integer: parseInt(req.body.integer),
         float: parseFloat(req.body.float),
         date: req.body.date,
-        boolean: JSON.parse(req.body.boolean)
+        boolean: req.body.boolean
     });
     writeData(data);
     res.redirect('/');
@@ -124,7 +145,6 @@ app.get('/edit/:id', function (req, res, next) {
 
 app.post('/edit/:id', function (req, res) {
     let idx = req.params.id;
-    console.log(req.body)
     data[idx] = {
         string: req.body.string,
         integer: parseInt(req.body.integer),
@@ -139,10 +159,19 @@ app.post('/edit/:id', function (req, res) {
 //Delete
 app.get('/delete/:id', function (req, res, next) {
     let data = JSON.parse(fs.readFileSync(path.join(__dirname, './db/data.json'), 'utf8'));
-    let id = req.params.id -1;
+    let id = req.params.id - 1;
     data.splice(id, 1);
     writeData(data);
     res.redirect('/');
 });
+
+app.get('/kaskus', function (req, res, next) {
+    const { count } = req.query
+    let kaskus = []
+    if (count) kaskus = getKasKus(count)
+    res.render('kaskus.ejs',{
+        kaskus
+    })
+})
 
 app.listen(port, () => { console.info(`Anda sedang berjalan di port ${port}`) })
